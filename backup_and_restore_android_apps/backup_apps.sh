e54bf15c4660b29c35a19512ebbc6313acd55829
kyberdrb <kyberdrb@gmail.com>
2021-11-29 22:20:47 +0100

    Update for Android 8 (Oreo)...
    ...Tested on Sony Xperia XA1
    TODO test data backup with a single package first, then scriptify - I'm not sure the data backup will work

--- backup_and_restore_android_apps/backup_apps.sh
+++ backup_and_restore_android_apps/backup_apps.sh
@@ -11,26 +11,26 @@ cd "${DESTINATION_DIR}"
 
 for app in $(adb shell pm list packages -f -3)
 do
-  apk_package_file="$(echo "${app}" | sed "s/^package://" | sed "s/=/ /" | cut -d' ' -f1 | tr -d '\r')"
-  apk_package_name="$(echo "${app}" | sed "s/^package://" | sed "s/=/ /" | cut -d' ' -f2 | tr -d '\r')"
-  
-  destination_apk_package_name="${apk_package_name}".apk
-  
-  apk_package_source_path=$(echo ${apk_package_file%=*})
+  package_info="$(echo "${app}" | sed "s/^package://")"
+  package_file_absolute_path_without_extension=$(echo ${package_info%\.apk=*})
+  package_file_absolute_path_with_extension="${package_file_absolute_path_without_extension}.apk"
 
-  echo "apk package file: ${apk_package_file}"
-  echo "destination apk package name: ${destination_apk_package_name}"
-  echo "destination data name for package: ${destination_data_name_for_package}"
-  echo "apk package name: ${apk_package_name}"
-  echo "apk package source path: ${apk_package_source_path}"
+  package_name="$(echo "${app}" | sed "s/^package://" | sed "s/=/ /g" | cut -d' ' -f4 | tr -d '\r')"
 
-  adb pull "${apk_package_file}" "${destination_apk_package_name}"
+  echo "package info: ${package_info}"
+  echo "package file absolute path with extension: ${package_file_absolute_path_with_extension}"
+  echo "apk package name: ${package_name}"
 
-  if [ "${BACKUP_WITH_DATA}" = "--with-data" ]
-  then 
-    destination_data_name_for_package="${apk_package_name}".data
-    adb backup -f "${destination_data_name_for_package}" -apk "${apk_package_name}"
-  fi
+  adb pull "${package_file_absolute_path_with_extension}" "${package_name}"
+
+  #echo "destination data name for package: ${destination_data_name_for_package}"
+
+  # TODO test data backup with a single package first, then scriptify - I'm not sure the data backup will work
+  #if [ "${BACKUP_WITH_DATA}" = "--with-data" ]
+  #then 
+  #  destination_data_name_for_package="${package_name}.data"
+  #  adb backup -f "${destination_data_name_for_package}" -apk "${package_name}"
+  #fi
 
   echo
   echo "----------------------------------------------------------"
