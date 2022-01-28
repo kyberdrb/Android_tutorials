4d446c3f07e95248a42b98a2219be543d90859b2
kyberdrb <kyberdrb@gmail.com>
2022-01-28 14:31:32 +0100

    Restore Vivaldi tabs to another Android phone

--- backup_and_restore_browser_tabs/backup_tabs.sh
+++ backup_and_restore_browser_tabs/backup_tabs.sh
@@ -34,8 +34,7 @@ do
   adb shell am start -W com.vivaldi.browser/com.google.android.apps.chrome.Main
   sleep 3
   
-  #adb shell input touchscreen tap 360 110
-  adb shell input touchscreen swipe 360 110 360 110 3000
+  adb shell input touchscreen swipe 360 130 360 130 3000
   sleep 3
   
   adb shell input touchscreen tap 565 325
@@ -87,11 +86,34 @@ do
 
   adb shell am start -W com.vivaldi.browser/com.google.android.apps.chrome.Main
   sleep 2
-  adb shell input touchscreen swipe 550 110 150 110 600
+  adb shell input touchscreen swipe 550 130 150 130 600
   sleep 15
 done
 
 # TODO test: Remove last duplicate URL
-#adb shell cat "${tabs_file_path}" | head -n -1
+adb shell cat "${tabs_file_path}" | head -n -1 > "/tmp/tabs.txt"
 
-# TODO Insert between links an empty new line
+# TODO Insert an empty new line in between each line with link
+adb shell cat "/tmp/tabs.txt" | sed -e 'G' > "/tmp/tabs.txt"
+
+adb push "/tmp/tabs.txt" "${tabs_file_path}"
+
+# README.md
+#Sources
+#- https://duckduckgo.com/?q=gesture+automate+android+adb&ia=web
+#- https://stackoverflow.com/questions/3437686/how-to-use-adb-to-send-touch-events-to-device-using-sendevent-command
+#- https://duckduckgo.com/?q=adb+gesture&ia=web&iax=qa
+#- https://stackoverflow.com/questions/39595431/some-swipe-gesture-with-adb-input/39595726#39595726
+#- https://stackoverflow.com/questions/4567904/how-to-start-an-application-using-android-adb-tools#4567928
+#- https://duckduckgo.com/?q=adb+show+intents+from+apk&ia=web&iax=qa
+#- https://sodocumentation.net/adb/topic/4252/showing-logs-on-adb
+#- All Android Key Events for usage with adb shell: https://gist.github.com/arjunv/2bbcca9a1a1c127749f8dcb6d36fb0bc
+#- https://duckduckgo.com/?q=adb+press+back+button&ia=web&iax=qa
+#- https://unix.stackexchange.com/questions/314486/shell-logical-not#314497
+#- https://stackoverflow.com/questions/3924182/how-does-the-keyword-if-test-if-a-value-is-true-or-false
+#- https://stackoverflow.com/questions/13542832/difference-between-single-and-double-square-brackets-in-bash
+#- https://stackoverflow.com/questions/3826425/how-to-represent-multiple-conditions-in-a-shell-if-statement
+#- https://duckduckgo.com/?q=posix+shell+compound+conditions+example&ia=web
+#- https://stackoverflow.com/questions/65514753/negate-a-boolean-variable-and-assign-it-to-a-new-variable
+#- https://duckduckgo.com/?q=delete+blank+lines+sed&ia=web
+#- https://unix.stackexchange.com/questions/207835/how-to-insert-multiple-blank-lines-after-every-line
---
+++ backup_and_restore_browser_tabs/restore_tab.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -x
+
+LINK="$1"
+
+# Optimized for
+#   - Samsung Galaxy J3 2016,
+#   - 720x1280 resolution,
+#   - Vivaldi browser
+
+# Pre-conditions
+#   - Vivaldi browser opened at blank tab overview, i.e. without tabs but one [because Vivaldi opens quick launch tab immediately after closing the last tab] - disable 'Show tab list' - then minimize the browser
+
+adb shell am start -W com.vivaldi.browser/com.google.android.apps.chrome.Main
+sleep 3
+
+# New tab
+adb shell input touchscreen tap 645 1230
+sleep 5
+
+# Activate address bar
+adb shell input touchscreen swipe 360 130 360 130 500
+sleep 3
+adb shell input touchscreen tap 360 130
+sleep 2
+
+# Enter link in address bar
+adb shell input text "${LINK}"
+sleep 1
+
+# Open link in address bar
+adb shell input keyevent KEYCODE_ENTER
+sleep 10
+
+# Open tab overview
+adb shell input touchscreen swipe 360 110 360 2000 2000
+
+# README.md
+#Sources
+#- 
