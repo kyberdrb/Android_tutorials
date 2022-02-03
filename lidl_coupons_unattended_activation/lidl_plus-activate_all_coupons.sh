87b2b1b1b08b01c4129b6f1971f28a2fd82bae23
kyberdrb <kyberdrb@gmail.com>
2022-02-03 13:46:19 +0100

    Clean up code

--- lidl_coupons_unattended_activation/lidl_plus-activate_all_coupons.sh
+++ lidl_coupons_unattended_activation/lidl_plus-activate_all_coupons.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-set -x
+#set -x
 
 echo "=============================================="
+echo
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo
 echo "DISABLE ANY BLUE LIGHT FILTER"
 echo "IN ORDER TO HAVE ACCURATE COLOR REPRESENTATION"
 echo "WHAT THIS SCRIPT RELIES UPON"
 echo "OTHERWISE IT MAY NOT FUNCTION PROPERLY"
+echo
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo
 echo "=============================================="
 
 # Launch Lidl Plus
@@ -16,13 +22,13 @@ echo "=============================================="
 
 adb shell am start -W com.lidl.eci.lidlplus/es.lidlplus.i18n.splash.presentation.view.SplashActivity
 
-sleep 10
-
+sleep 7
+ 
 # Go to tab 'Kupóny'
 
 adb shell input touchscreen tap 215 1215
 
-sleep 5
+sleep 4
 
 # Go to the end of the coupons list
 
@@ -94,9 +100,7 @@ do
   # Wait - for debugging purposes
   #sleep 3
 
-  # click the button: find the area; check color of deactivated or locked coupon; activate coupon if deactivated; go to the next coupon when current coupon is marked as locked
-
-  begin_y=913
+  begin_y=930
   offset=0
 
   while true
@@ -122,47 +126,17 @@ do
       # if the coupon is deactivated
       if [ "${inner_color}" = "0050AA00" ]
       then
-        # open coupon
-        #adb shell input touchscreen swipe 640 $(( y_coordinate - 100 )) 640 $(( y_coordinate - 100 )) 3000
-
         # activate the coupon
         #adb shell input touchscreen swipe 640 ${inner_y_coordinate} 640 ${inner_y_coordinate} 5000
         adb shell input touchscreen tap 640 ${inner_y_coordinate}
 
         sleep 3
-        
-        #adb shell input touchscreen swipe 675 830 675 830 3000
-        # TODO make two screenshots
-        # TODO glue the screenshots together
-        # TODO extract text from the screenshots
-        # TODO parse extracted text to form a valid special offer item
-        #
-        # go back to the list of coupons
-        #adb shell input keyevent KEYCODE_BACK
-        
-        #sleep 4
 
         break
       fi
 
-      # if the coupon is activated
       if [ "${inner_color}" = "FFFFFFFF" ]
       then
-        # open coupon
-        #adb shell input touchscreen swipe 640 $(( y_coordinate - 100 )) 640 $(( y_coordinate - 100 )) 3000
-
-        #sleep 5
-
-        # TODO make two screenshots
-        # TODO glue the screenshots together
-        # TODO extract text from the screenshots
-        # TODO parse extracted text to form a valid special offer item
-        #
-        # go back to the list of coupons
-        #adb shell input keyevent KEYCODE_BACK
-
-        #sleep 5
-
         break
       fi
     fi
@@ -170,69 +144,9 @@ do
     offset=$(( offset + 1 ))
   done
 
-  adb shell input touchscreen swipe 0 500 0 935 2000
+  adb shell input touchscreen swipe 0 500 0 935 1200
+
+  sleep 1
 
   screenshot_number=$(( screenshot_number + 1 ))
 done
-
-# TEST 8 - 434 - 1 = 433
-#
-# 
-
-# TEST 7 - 435 - 1 = 434
-#
-# LOWEST DEVIANCE! CHOOSING...
-
-# TEST 6 - 436 - 1 = 435
-#
-# EVEN EVEN BETTER???
-
-# TEST 5 - 437 - 1 = 436
-#
-# EVEN BETTER???
-
-# TEST 4 - 434 + 3 = 437
-#
-# OK?
-
-# TEST 3 - 438 - 4 = 434
-#
-# 913 - 910 = 3
-
-# TEST 2 - 425 + 13 = 438
-#
-#[laptop@laptop Downloads]$ magick "${HOME}/screenshot-phone-438-0.png" -format "%[hex:u.p{640,913}]\n" info:
-#80285580
-#[laptop@laptop Downloads]$ magick "${HOME}/screenshot-phone-438-0.png" -format "%[hex:u.p{640,912}]\n" info:
-#80285580
-#[laptop@laptop Downloads]$ magick "${HOME}/screenshot-phone-438-0.png" -format "%[hex:u.p{640,914}]\n" info:
-#EA6A6AEA
-#[laptop@laptop Downloads]$ magick "${HOME}/screenshot-phone-438-0.png" -format "%[hex:u.p{640,913}]\n" info:
-#80285580
-#[laptop@laptop Downloads]$ magick "${HOME}/screenshot-phone-438-1.png" -format "%[hex:u.p{640,913}]\n" info:
-#80285580
-#[laptop@laptop Downloads]$ magick "${HOME}/screenshot-phone-438-1.png" -format "%[hex:u.p{640,917}]\n" info:
-#80285580
-#[laptop@laptop Downloads]$ magick "${HOME}/screenshot-phone-438-1.png" -format "%[hex:u.p{640,916}]\n" info:
-#80285580
-#[laptop@laptop Downloads]$ magick "${HOME}/screenshot-phone-438-1.png" -format "%[hex:u.p{640,918}]\n" info:
-#EA6A6AEA
-#[laptop@laptop Downloads]$ echo $(( 917 - 913))
-#4
-
-# TEST 1 - 425
-#
-#[laptop@laptop Downloads]$ magick "${HOME}/screenshot-phone-425-0.png" -format "%[hex:u.p{640,913}]\n" info:
-#80285580
-#[laptop@laptop Downloads]$ magick "${HOME}/screenshot-phone-425-1.png" -format "%[hex:u.p{640,913}]\n" info:
-#FF7F7FFF
-#[laptop@laptop Downloads]$ magick "${HOME}/screenshot-phone-425-1.png" -format "%[hex:u.p{640,914}]\n" info:
-#FF7F7FFF
-#[laptop@laptop Downloads]$ magick "${HOME}/screenshot-phone-425-1.png" -format "%[hex:u.p{640,915}]\n" info:
-#FF7F7FFF
-#[laptop@laptop Downloads]$ magick "${HOME}/screenshot-phone-425-1.png" -format "%[hex:u.p{640,901}]\n" info:
-#EB6B6BEB
-#[laptop@laptop Downloads]$ magick "${HOME}/screenshot-phone-425-1.png" -format "%[hex:u.p{640,900}]\n" info:
-#80285580
-#[laptop@laptop Downloads]$ echo $(( 425 + 13 ))
-#438
